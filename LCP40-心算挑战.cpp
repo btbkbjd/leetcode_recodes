7afa48b8a33123abb7e6edccec74849c286cab1e
unknown <970590123@qq.com>
2024-08-03 21:22:46 +0800

    feat: 把4个最值改成两个最值数组，写sum的时候更优雅了

--- LCP40-心算挑战.cpp
+++ LCP40-心算挑战.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
     int maxmiumScore(vector<int>& cards, int cnt) {
-        int min_odd = INT_MAX, min_even = INT_MAX, max_even = INT_MIN, max_odd = INT_MIN;
+        // ÷2 单纯为了之后在sum里做加减法的时候不溢出。
+        int min_ele[2] = {INT_MAX/2, INT_MAX/2};
+        int max_ele[2] = {INT_MIN/2, INT_MIN/2};
         int N = cards.size();
-        ranges::sort(cards, greater());
-        int sum = reduce(cards.begin(), cards.begin() + cnt, 0);
-        
+        /* nth_element:使得某个元素位于其最终排序位置上，左侧的元素不大于它，右侧的元素不小于它。
+         * 这里结合投影使用，就可以实现倒序。
+         */
+        ranges::nth_element(cards, cards.begin() + cnt, std::greater<int>()); 
+        int sum = reduce(cards.begin(), cards.begin() + cnt, 0); // 最大的 cnt 个数之和
         if(sum % 2 == 0) return sum; 
         
         for(int i = 0; i < cnt; ++i){
-            if(cards[i] % 2 != 0) min_odd = cards[i];
-            else min_even = cards[i];
+            int v = cards[i];
+            min_ele[v % 2] = min(min_ele[v % 2], v);// 最小的被替换掉的奇数为min_ele[1],偶数为min_ele[0]
         }
-        for(int i = N - 1; i >= cnt; --i){
-            if(cards[i] % 2 != 0) max_odd = cards[i];
-            else max_even = cards[i];
+        for(int i = cnt; i < N; ++i){
+            int v = cards[i];
+            max_ele[v % 2] = max(max_ele[v % 2], v);// 最大的被替换进来的奇数为max_ele[1],偶数为max_ele[0]
         }
 
-        int t1 = max_odd != INT_MIN && min_even != INT_MAX ? sum - min_even + max_odd : 0;
-        int t2 = max_even != INT_MIN && min_odd != INT_MAX ? sum - min_odd + max_even : 0;
-        sum = max(t1, t2);
-
+        // 如果没有出现max_ele[0]或max_ele[1]之一，则为0；如果没有出现min_ele[0]或min_ele[1]之一，则为0，很巧
+        sum = max(sum + max(max_ele[0] - min_ele[1], max_ele[1] - min_ele[0]), 0);
         return sum;
     }
 };
