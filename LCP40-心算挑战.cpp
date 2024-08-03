6c20f94722c2a0350198e5e77bd4afcc928a093c
unknown <970590123@qq.com>
2024-08-03 17:51:48 +0800

    feat: 1. sort和accumulate用 cpp 17 和 cpp20的语法糖替换了一下； 2. 4个最值用INT_MIN,INT_MAX代替，最后写sum会优雅一些。

--- LCP40-心算挑战.cpp
+++ LCP40-心算挑战.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxmiumScore(vector<int>& cards, int cnt) {
-        int min_odd = 0, min_even = 0, max_even = 0, max_odd = 0, sum = 0;
+        int min_odd = INT_MAX, min_even = INT_MAX, max_even = INT_MIN, max_odd = INT_MIN;
         int N = cards.size();
-        sort(cards.rbegin(), cards.rend());
+        ranges::sort(cards, greater());
+        int sum = reduce(cards.begin(), cards.begin() + cnt, 0); 
         for(int i = 0; i < cnt; ++i){
-            sum += cards[i];
             if(cards[i] % 2 != 0) min_odd = cards[i];
             else min_even = cards[i];
         }
@@ -15,12 +15,9 @@ public:
         }
         if(sum % 2 != 0)
         {
-            int t1 = max_odd - min_even;
-            int t2 = max_even - min_odd;
-            if(max_even && max_odd && min_even && min_odd) sum += max(t2, t1);
-            else if(max_odd && min_even) sum += t1;
-            else if(max_even && min_odd) sum += t2;
-            else sum = 0;
+            int t1 = max_odd != INT_MIN && min_even != INT_MAX ? sum - min_even + max_odd : 0;
+            int t2 = max_even != INT_MIN && min_odd != INT_MAX ? sum - min_odd + max_even : 0;
+            sum = max(t1, t2);
         } 
         return sum;
     }
